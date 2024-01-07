9f1c9eabb6a9c9eaaf5c29efb1a51cd701873738
irfanieka <65238670+irfanieka@users.noreply.github.com>
2024-01-07 11:48:38 +0700

    Update cek.sh

--- cek.sh
+++ cek.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-POOL=138.197.98.18:80
-WALLET=solo:pyrin:qzyahkcpgpvjk4u50yrajn62nke52vgn4vwmz3au29u3ufkvujlewkftlg7s5
+POOL=pyi.kryptex.network:7777
+WALLET=pyrin:qzqnxs45294g5fgvyf5ak2zgz35m8cfzg4xe9al6gvh292ys040mz4gxg2u4k
 
 ./node --algo PYRIN --pool $POOL --user $WALLET $@ --no-cl
