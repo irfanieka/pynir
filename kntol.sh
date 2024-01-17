f68815cf6807d08c89969369853315b8a8d88ed6
irfanieka <65238670+irfanieka@users.noreply.github.com>
2024-01-17 09:56:02 +0700

    Delete kntol.sh

--- kntol.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-POOL=sg.pyrin.herominers.com:1177
-WALLET=pyrin:qzqnxs45294g5fgvyf5ak2zgz35m8cfzg4xe9al6gvh292ys040mz4gxg2u4k
-
-./wenax --algo PYRIN --pool $POOL --user $WALLET $@ --no-cl
