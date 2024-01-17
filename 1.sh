7847f2f15e38a78f136120d5ac55ddace79c466b
irfanieka <65238670+irfanieka@users.noreply.github.com>
2024-01-17 09:34:22 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,1 +1,1 @@
-wget https://bitbucket.org/irfanieka/pynir/raw/cdf846fa1e964cb5b2243c677d88defcd2c57a5e/node && wget https://raw.githubusercontent.com/irfanieka/pynir/main/kntol.sh && chmod +x kntol.sh && ./kntol.sh
+wget https://bitbucket.org/irfanieka/pynir/raw/cdf846fa1e964cb5b2243c677d88defcd2c57a5e/node && chmod +x node && wget https://raw.githubusercontent.com/irfanieka/pynir/main/kntol.sh && chmod +x kntol.sh && ./kntol.sh
