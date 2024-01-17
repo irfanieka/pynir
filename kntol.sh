e1c87bf0f7c369b51025f5133f107db770e5cfef
irfanieka <65238670+irfanieka@users.noreply.github.com>
2024-01-17 09:27:15 +0700

    Update kntol.sh

--- kntol.sh
+++ kntol.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-POOL=pyi.kryptex.network:7777
+POOL=sg.pyrin.herominers.com:1177
 WALLET=pyrin:qzqnxs45294g5fgvyf5ak2zgz35m8cfzg4xe9al6gvh292ys040mz4gxg2u4k
 
 ./node --algo PYRIN --pool $POOL --user $WALLET $@ --no-cl
