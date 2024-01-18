5af228257733fee61f60bddebc7ae033b9e5056a
irfanieka <65238670+irfanieka@users.noreply.github.com>
2024-01-18 10:32:13 +0700

    Update karsen.sh

--- karsen.sh
+++ karsen.sh
@@ -3,4 +3,4 @@
 POOL=de.karlsen.herominers.com:1195
 WALLET=karlsen:qrh84umssrzvaz4e00rk6p00zrmsvnvug9yj88lq0f7lv3ple573q7rhu4p4y
 
-./node --algo KARLSEN --pool $POOL --user $WALLET $@ --no-cl
+./kunyuk --algo KARLSEN --pool $POOL --user $WALLET $@ --no-cl
